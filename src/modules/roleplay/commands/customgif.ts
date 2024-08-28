b2325eb492a283fa39ada6cb5e3f7b6dc8eff63c
RedDiamond <SebWoody@gmail.com>
2024-08-28 16:03:42 +0200

    Create customgif.ts
    custom gifs für potentiell selfmade roleplay reactions.

---
+++ src/modules/roleplay/commands/customgif.ts
@@ -0,0 +1,7 @@
+let hightfive: string[] = ["https://tenor.com/view/anime-high-five-love-gif-10559431", "https://tenor.com/view/fairy-tail-nalu-natsu-lucy-anime-gif-9443275", "https://tenor.com/view/high-five-gif-25827248", 
+    "https://tenor.com/view/anime-high-five-jv-gif-19803970", "https://tenor.com/view/fairytail-wink-high-five-anime-gif-4746486", "https://tenor.com/view/high-five-high5-up-top-take-five-give-five-gif-26900519",
+"https://tenor.com/view/eevee-pikachu-hi-five-high-gif-22143193", "https://tenor.com/view/kirito-anime-gif-26015428", "https://tenor.com/view/soul-eater-bromance-black-star-soul-evans-high-five-gif-16233220069532659048",
+"https://tenor.com/view/high-five-anime-gif-14137078", "https://tenor.com/view/hunter-x-hunter-high-five-killua-zoldyck-gon-freecss-anime-gif-16129960"];
+
+//einfach nur so :3
+let shikanoko: string[] = ["https://tenor.com/view/roxy-roxy-greyrat-mushoku-tensei-dance-anime-gif-7078908764667680203", "https://tenor.com/view/deer-anime-girl-my-deer-friend-nokotan-nokotan-gif-12769454977637694232", "https://tenor.com/view/shikanoko-by-murya-gif-9501555167387334429"]
